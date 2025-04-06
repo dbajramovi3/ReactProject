ff591e713dd13174ecda42f41ddc035c095213e5
dbajramovi3 <dbajramovi3@etf.unsa.ba>
2025-04-06 15:34:32 +0200

    ispravka za deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
+
 import tailwindcss from '@tailwindcss/vite'
+
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(), ],
-  base: process.env.VITE_BASE_PATH || "/ReactProject"
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react(), tailwindcss(), ],
+    base: env.VITE_BASE_PATH || "/",
+  }
 })
+
